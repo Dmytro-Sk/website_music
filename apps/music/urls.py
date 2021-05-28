f37e294dbcc07e756486d019a46e74e224a4c227
Dmytro-Sk <0971898920d@gmail.com>
2021-05-28 14:24:43 +0300

    Add some changes - add logic to detail.html - make dynamic url in index.html - change def in views.py

--- apps/music/urls.py
+++ apps/music/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 app_name = 'music'
 urlpatterns = [
-    path('', views.index),
-    path('<int:album_id>/', views.detail),
+    path('', views.index, name='index'),
+    path('<int:album_id>/', views.detail, name='detail'),
 ]
