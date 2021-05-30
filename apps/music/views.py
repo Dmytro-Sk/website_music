ce78ee2caa3febd40838fe047f826a0c77620c10
Dmytro-Sk <0971898920d@gmail.com>
2021-05-30 13:54:05 +0300

    Add html templates

--- apps/music/views.py
+++ apps/music/views.py
@@ -5,12 +5,12 @@ from .models import Album, Song
 
 def index(request):
     all_albums = Album.objects.all()
-    return render(request, 'music/index.html', {'all_albums': all_albums})
+    return render(request, 'music/index.html', {'all_albums': all_albums, 'title': 'All Albums'})
 
 
 def detail(request, album_id):
     album = get_object_or_404(Album, id=album_id)
-    return render(request, 'music/detail.html', {'album': album})
+    return render(request, 'music/detail.html', {'album': album, 'title': 'Album Details'})
 
 
 def favorite(request, album_id):
